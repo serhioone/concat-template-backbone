4123dedd880a78ace1389f6958184b1074900814
serhioone <serhioone@gmail.com>
2016-06-07 12:15:15 +0300

    new compile

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var through = require('through2'),
 module.exports = function(fileName, options) {
 
     options = options || {
-        prefix: "Vtb.template"
+        prefix: "Vtb.template",
+        dirName: false
     };
 
     var firstFile = null;
@@ -30,19 +31,49 @@ module.exports = function(fileName, options) {
             firstFile = file;
         }
 
-        var methodName = file.relative.split('.');
-        var dirName = file.base.split("templates/")[1].split("/");
-        var dj = dirName.length - 2;
-
-        text += "\n\t" + options.prefix + "." + dirName[dj] + "." + methodName[0] + " = '"
+        var methodName = path.basename(file.path).split('.');
+        if(options.dirName) {
+            var dirName = file.base.split("templates/")[1].split("/");
+            var dj = dirName.length - 2;
+        }
+        if(options.dirName) {
+            text += "\n\t" + options.prefix + "." + dirName[dj] + "." + methodName[0] + " = '";
+        } else {
+            text += "\n\t" + options.prefix + "." + methodName[0] + " = '";
+        }
+        
         text += "<script>" + file.contents.toString().replace(/(\r\n|\n|\r|\t)/gm,"").replace(/\s{2,}/g, ' ') + "</script>";
         text += "';";
+
+        // minify(file.path, function(error, data) {
+        //     if (error) {
+        //         console.error(error.message);
+        //     } else {
+        //         console.log(data);
+        //         text += data;
+        //         // console.log(text);
+        //         // console.log("------");
+        //     }
+        // });
+
+        // console.log(compileText);
+
+        
 
         callback();
     };
 
     function endStream(callback) {
         
+        // console.log(text += footer);
+
+        // exportFile.relative = "test.js";
+        // exportFile.contents = text + footer;
+
+        // console.log(exportFile);
+        // this.push(text + footer);
+
+        //Configure outgoing file.
         var joinedPath = path.join(firstFile.base, fileName);
 
         var joinedFile = new File({
@@ -52,6 +83,7 @@ module.exports = function(fileName, options) {
             ,contents: new Buffer(text + footer)
         });
 
+        // console.log(joinedFile);
         this.push(joinedFile);
         callback();
     }
